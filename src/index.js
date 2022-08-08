138b473b3f3e608942f93b96b08ec4b7f7a0e377
MercedesTello <mtelloespana@gmail.com>
2022-08-08 10:40:44 -0500

    terminado

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import validator from './validator.js';
+const validador= document.getElementById("botonValidar");
+const resultado= document.getElementById("resultado");
+    validador.addEventListener("click", function() {
+        const creditCardNumber= document.getElementById("tarjeta").value; //se selecciona todo el valor dentro del elemento
+        
+        const validar= validator.isValid(creditCardNumber)
+        if (validar==true){
+            resultado.innerHTML=`<h3 class="valida"> ¡Felicidades! Tu compra es un éxito</h3>`
+        }else{
+            resultado.innerHTML=`<h3 class="invalida"> ¡Ups! Tu tarjeta ha sido rechazada</h3>`
+        }      
+    })
 
-console.log(validator);
+    validador.addEventListener("click", function() {
+    let creditCardNumber= document.getElementById("tarjeta");
+    let creditCardMaskify= validator.maskify(creditCardNumber.value);
+    console.log (creditCardMaskify)
+    creditCardNumber.value= creditCardMaskify;
+    console.log(creditCardNumber);
+      //incertar creditcardmaskify en mi elemento con id tarjeta
+    })
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,51 @@
 const validator = {
-  // ...
-};
+  isValid: function (creditCardNumber) {
+    let arrCreditCardNumberReverse = creditCardNumber.split("").reverse();
+    let sum = 0;
+    for (let i = 0; i < arrCreditCardNumberReverse.length; i++) {
+      // 4356
+      // 0123
+      const numberString = arrCreditCardNumberReverse[i];
+      let number = 0;
+      //los impares
+      if (i % 2 == 0) {
+        number = parseInt(numberString);
+      }
+      // aca van los pares
+      else {
+        number = parseInt(numberString) * 2;
+        if (number >= 10) {
+          let strNumber = number.toString()
+          number = parseInt(strNumber[0]) + parseInt(strNumber[1])
+        }
+      }
+      sum = sum + number
+    }
+    if ((sum % 10) == 0) {
+      return true
+    } else {
+      return false
+    }
+    
+  },
+  maskify:function (tarjeta){
+    if(tarjeta.length <=4){ 
+      return tarjeta
+    }
+    else{//transformar de tarjeta los 12 primeros digitos en # y concatenarlo con los ultimos 4 números
+      let guardar= tarjeta.slice(-4)
+      let masked= tarjeta.slice(0, -4)
+      let cat= ""
+      for (let i=0; i< masked.length;i++){
+      cat+="#" 
+      }
+      console.log (masked)
+      return cat+=guardar
+    }
+  }
+  }
+
+
+//validator.isValid('4137894711755904'); // true
 
 export default validator;
